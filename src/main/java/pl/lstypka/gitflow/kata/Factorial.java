a5adabb48ed0878137b2438e9c6104eebc8d33f6
Lukasz Stypka <lukasz.stypka@gmail.com>
2016-11-23 21:15:55 +0100

    KATA-2 Clean code

--- src/main/java/pl/lstypka/gitflow/kata/Factorial.java
+++ src/main/java/pl/lstypka/gitflow/kata/Factorial.java
@@ -14,8 +14,12 @@ public class Factorial
 	public static long factorial(final long number)
 	{
 		if (number <= 1)
+		{
 			return 1;
+		}
 		else
+		{
 			return number * factorial(number - 1);
+		}
 	}
 }
